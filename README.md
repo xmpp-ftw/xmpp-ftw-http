8c33acce20ef841a388d2ee15121c33829171134
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-04-20 12:16:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+xmpp-ftw-http
+===================
+
+Verifying HTTP Requests via XMPP (XEP-0070) plugin for XMPP-FTW.
+
+https://github.com/xmpp-ftw/xmpp-ftw.git
+
+# More....
+
+See the XMPP-FTW demo repository - https://github.com/xmpp-ftw/xmpp-ftw-demo
+
+# Build status
+
+[![Build Status](https://secure.travis-ci.org/xmpp-ftw/xmpp-ftw-command.png)](http://travis-ci.org/xmpp-ftw/xmpp-ftw-http)
+
+# Install
+
+```
+npm i --save xmpp-ftw-http
+```
+
+# Test
+
+```
+npm test
+```
---
+++ test/lib/http.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/* jshint -W030 */
+
+var Http = require('../../index')
+  , helper = require('../helper')
+
+describe('HTTP Auth', function() {
+
+    var http, socket, xmpp, manager
+
+    var namespace = 'http://jabber.org/protocol/http-auth'
+
+    before(function() {
+        socket = new helper.SocketEventer()
+        xmpp = new helper.XmppEventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                if (typeof id !== 'object')
+                    throw new Error('Stanza ID spoofing protection not in place')
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            },
+            fullJid: {
+                local: 'user',
+                domain: 'example.com',
+                resource: 'laptop'
+            },
+            _getLogger: function() {
+                return {
+                    log: function() {},
+                    error: function() {},
+                    warn: function() {},
+                    info: function() {}
+                }
+            }
+        }
+        http = new Http()
+        http.init(manager)
+    })
+
+    beforeEach(function() {
+        socket.removeAllListeners()
+        xmpp.removeAllListeners()
+        http.init(manager)
+    })
+
+    it('Has the correct namespace', function() {
+        http.NS.should.equal(namespace)
+    })
+
+})
