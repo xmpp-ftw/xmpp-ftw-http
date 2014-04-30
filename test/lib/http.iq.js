596a8c8d6e8722fa387c9391e5a44c6061f8eb51
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-04-30 22:43:16 +0100

    IQ handling complete

--- test/lib/http.iq.js
+++ test/lib/http.iq.js
@@ -243,6 +243,169 @@ describe('HTTP Auth', function() {
         
         describe('Deny', function() {
             
+            it('Errors if callback provided but not a function', function(done) {
+                var request = {}
+                socket.on('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Callback should be a function')
+                    done()
+                })
+                socket.send('xmpp.http.deny', request, true)
+            })
+            
+            it('Errors if missing \'type\'', function(done) {
+                var request = {}
+                socket.on('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing \'type\' parameter')
+                    done()
+                })
+                socket.send('xmpp.http.deny', request)
+            })
+            
+            it('Errors on invalid \'type\'', function(done) {
+                var request = { type: 'presence' }
+                socket.on('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Invalid \'type\' parameter')
+                    done()
+                })
+                socket.send('xmpp.http.deny', request)
+            })
+            
+            it('Errors if \'id\' is missing', function(done) {
+                var request = { type: 'iq' }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing \'id\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
+            it('Errors if missing \'to\'', function(done) {
+                var request = { type: 'iq', id: '1' }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing \'to\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
+            it('Errors if \'to\' is empty', function(done) {
+                var request = { type: 'iq', id: '1', to: '' }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing \'to\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
+            it('Errors if \'request\' but not \'url\'', function(done) {
+                var request = { type: 'iq', id: '1', to: 'you', request: {} }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing request \'url\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+
+            it('Errors if \'request\' but not \'id\'', function(done) {
+                var request = { type: 'iq', id: '1', to: 'you', request: { url: 'http' } }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing request \'id\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+
+            it('Errors if \'request\' but not \'method\'', function(done) {
+                var request = {
+                    type: 'iq',
+                    id: '1',
+                    to: 'you',
+                    request: { url: 'http', id: 2 }
+                }
+                var callback = function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.request.should.eql(request)
+                    error.description.should.equal('Missing request \'method\' parameter')
+                    done()
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
+            it('Sends expected minimal response', function(done) {
+                var request = { type: 'iq', id: '1', to: 'you' }
+                xmpp.on('stanza', function(stanza) {
+                    stanza.is('iq').should.be.true
+                    stanza.attrs.to.should.equal(request.to)
+                    stanza.attrs.id.should.equal(request.id)
+                    stanza.attrs.type.should.equal('error')
+                    var error = stanza.getChild('error')
+                    error.should.exist
+                    error.attrs.type.should.equal('auth')
+                    error.getChild('not-authorized', http.NS_ERROR).should.exist
+                    done()
+                })
+                var callback = function(error, success) {
+                    should.not.exist(error)
+                    success.should.be.true
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
+            it('Sends expected response with <confirm/>', function(done) {
+                var request = {
+                    type: 'iq',
+                    id: '1',
+                    to: 'you',
+                    request: { id: '5', url: 'http', method: 'GET' }
+                }
+                xmpp.on('stanza', function(stanza) {
+                    stanza.is('iq').should.be.true
+                    stanza.attrs.to.should.equal(request.to)
+                    stanza.attrs.id.should.equal(request.id)
+                    stanza.attrs.type.should.equal('error')
+                    var confirm = stanza.getChild('confirm', http.NS)
+                    confirm.should.exist
+                    confirm.attrs.url.should.equal(request.request.url)
+                    confirm.attrs.method.should.equal(request.request.method)
+                    confirm.attrs.id.should.equal(request.request.id)
+                    var error = stanza.getChild('error')
+                    error.should.exist
+                    error.attrs.type.should.equal('auth')
+                    error.getChild('not-authorized', http.NS_ERROR).should.exist
+                    done()
+                })
+                var callback = function(error, success) {
+                    should.not.exist(error)
+                    success.should.be.true
+                }
+                socket.send('xmpp.http.deny', request, callback)
+            })
+            
         })
 
     })
